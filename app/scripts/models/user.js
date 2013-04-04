86a863de12299bd1b7d6ddffb3a9d2095ae32bd3
Ikuo Terado <eqobar@gmail.com>
2013-04-05 00:15:19 +0900

    Removed email from user model

--- app/scripts/models/user.js
+++ app/scripts/models/user.js
@@ -5,7 +5,6 @@ define(['backbone'], function (Backbone) {
     var User = Backbone.Model.extend({
         defaults: {
             nickname: '',
-            email: '',
             password: '',
             password2: ''
         },
